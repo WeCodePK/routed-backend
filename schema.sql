2e9e6c7c483e48b5a1cdaa60bf5e1eb86663e642
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2025-07-27 04:54:34 +0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `admin_profiles` (
   `password_hash` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE IF NOT EXISTS `drivers` (
   `id` INT NOT NULL AUTO_INCREMENT,
