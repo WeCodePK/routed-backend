d442386a03d16ef2a81e30d295c770732f6e5a6c
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2025-07-29 05:47:35 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,36 +1,24 @@
 const express = require('express');
-const dbPool = require('./database');
-const { authMiddleware } = require('./util');
-const cors = require('cors');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+const { auth } = require('./util');
+app.locals.db = require('./database');
 
-app.use(morgan('dev'));
+app.use(cors());
+app.use(morgan());
 app.use(express.json());
-
-app.locals.dbPool = dbPool;
-
-app.use(cors({
-    origin: 'https://routed-web.wckd.pk',
-    methods: ['Get', 'Post', 'Put', 'Delete'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-    optionsSuccessStatus: 200,
-}))
 
 app.get('/', (req, res) => {
     res.sendStatus(200);
 });
 
 app.use('/api/v0/auth', require('./controllers/authController'));
-app.use('/api/v0/routes', authMiddleware, require('./controllers/routesController'));
-app.use('/api/v0/drivers', authMiddleware, require('./controllers/driversController'));
-app.use('/api/v0/tracking', authMiddleware, require('./controllers/trackingController'));
-app.use('/api/v0/violations', authMiddleware, require('./controllers/violationsController'));
-app.use('/api/v0/admin/profile', authMiddleware, require('./controllers/profileController'));
-app.use('/api/v0/assignments', authMiddleware, require('./controllers/assignmentsController'));
+app.use('/api/v0/routes', auth, require('./controllers/routesController'));
+app.use('/api/v0/drivers', auth, require('./controllers/driversController'));
+app.use('/api/v0/assignments', auth, require('./controllers/assignmentsController'));
 
-app.listen(80, () => {
-    console.log(`Server is running`);
+app.listen(3000, () => {
+    console.log(`[INFO] Server is running`);
 });
