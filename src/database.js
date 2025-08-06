d925747d2f54ee2e357f3cdf1b21f41603293014
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2025-08-06 08:51:15 +0500

    connect using connection string

--- src/database.js
+++ src/database.js
@@ -2,15 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const pool = mysql.createPool(process.env.MYSQL_URL);
 
 async function initializeDatabase() {
   try {
